4fac1576d99c35ae29b56d945fae5e111347dc8b
Jerem <jersou@gmail.com>
2023-04-14 00:00:51 +0200

    feat: config editor: init Storybook

---
+++ config-editor/src/components/ScreenEdges.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ScreenEdges } from './ScreenEdges';
+
+const meta = {
+  title: 'ScreenEdges',
+  component: ScreenEdges,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof ScreenEdges>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Left: Story = { args: { edges: ["Left"] }};
+export const TopLeft: Story = { args: { edges: ["Top","Left"] }};
+
+export const Empty: Story = { args: { edges: [] }};
+
+export const Test = ()=> <div>→<ScreenEdges edges={["Top","Left"]}/>←</div>
